644c2a11e40e92fc11e8f6f370deaa7c5575a927
almacasea <almacasea@gmail.com>
2024-04-25 18:47:29 -0600

    Último Push

---
+++ src/main/java/com/tienda/service/CorreoService.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.tienda.service;
+
+import jakarta.mail.MessagingException;
+
+/**
+ *
+ * @author taraz
+ */
+public interface CorreoService {
+    
+    public void EnviarCorreoHTML(
+        String para, 
+        String asunto, 
+        String contenidoHTML)
+        throws MessagingException;
+}
